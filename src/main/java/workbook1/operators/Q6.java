3423f532f92c001d541d1fc0c60e1c58b0afe567
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 22:44:17 +0900

    feat: [operators] Q6 구현

--- src/main/java/workbook1/operators/Q6.java
+++ src/main/java/workbook1/operators/Q6.java
@@ -8,5 +8,12 @@ public class Q6 {
         int x = sc.nextInt();
         int y = sc.nextInt();
         // TODO: x와 y 중 큰 값에 5를 곱해 출력
+
+        //삼항 연산자 ? : 를 이용하여 연산
+        int result = ((x > y) ? x : y) * 5;
+
+        System.out.println(result);
+
+        sc.close();
     }
 }
