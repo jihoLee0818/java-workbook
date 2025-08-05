e683a0a49ef29c741c987420f7f0432ee9658699
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 22:58:56 +0900

    feat: [conditionals] Q1 구현

--- src/main/java/workbook1/conditionals/Q1.java
+++ src/main/java/workbook1/conditionals/Q1.java
@@ -7,5 +7,15 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         // TODO: num의 부호를 판단하여 출력
+
+        //if - else if - else문을 사용하여 음수, 0, 양수 출력
+        if (num < 0) {
+            System.out.println("음수");
+        } else if (num == 0) {
+            System.out.println("0");
+        }  else {
+            System.out.println("양수");
+        }
+        sc.close();
     }
-}
+    }
