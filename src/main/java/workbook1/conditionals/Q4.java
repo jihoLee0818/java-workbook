748790915423135c11019768b0e1ad48f2342264
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 23:18:22 +0900

    feat: [conditionals] Q4 구현

--- src/main/java/workbook1/conditionals/Q4.java
+++ src/main/java/workbook1/conditionals/Q4.java
@@ -7,5 +7,15 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
         // TODO: 윤년 판별 로직 구현
+
+        //중첩 조건문을 사용하여 윤년 판별 로직 작성
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("윤년");
+        } else {
+            //윤년이 아닐 경우 평년 출력
+            System.out.println("평년");
+        }
+
+        sc.close();
     }
 }
