6307e91100410bd7c98c4fa2d55bec4d8038c007
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 22:30:09 +0900

    feat: [operators] Q3 구현

--- src/main/java/workbook1/operators/Q3.java
+++ src/main/java/workbook1/operators/Q3.java
@@ -7,6 +7,14 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
         double y = sc.nextDouble();
-        // TODO: x &gt; y 결과와 !(x &gt; y) 결과 출력
+        // TODO: x > y 결과와 !(x < y) 결과 출력
+
+        //첫 번째 값이 두 번째 값보다 큰지 비교
+        boolean a = (x > y);
+        //첫 번째 결과에 NOT 연산을 적용
+        boolean b = !a;
+
+        //두 결과 출력
+        System.out.println(a + " " + b);
     }
 }
