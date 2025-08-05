79124c8f11a5c36021bd83de04953802ce3f4d39
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 21:28:45 +0900

    feat: [primitiveconversion] Q3 구현

--- src/main/java/workbook1/primitiveconversion/Q3.java
+++ src/main/java/workbook1/primitiveconversion/Q3.java
@@ -10,5 +10,14 @@ public class Q3 {
         int i = sc.nextInt();
         long l = sc.nextLong();
         // TODO: b, s, i, l을 long으로 변환해 합산 후 float으로 변환하여 출력
+
+        //long으로 자동 형변환 후 합 구하기
+        long longResult = b + s + i + l;
+
+        //long 결과를 float형으로 다시 변환하여 출력
+        float floatResult = longResult;
+        System.out.println(floatResult);
+
+        sc.close();
     }
 }
