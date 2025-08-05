33f81e0b0ba3b95176055e6d80d0315f410abca5
jihoLee0818 <leejiho0114@gmail.com>
2025-08-05 23:13:20 +0900

    feat: [conditionals] Q3 구현

--- src/main/java/workbook1/conditionals/Q3.java
+++ src/main/java/workbook1/conditionals/Q3.java
@@ -9,5 +9,23 @@ public class Q3 {
         int s2 = sc.nextInt();
         int s3 = sc.nextInt();
         // TODO: 평균을 계산하고 중첩 if-else로 학점 출력
+        
+        //세 과목 평균 구하기
+        double avg =(s1 + s2 + s3)/3.0;
+        
+        //if-else구조를 이용하여 A부터 순서대로 검사하기
+        if(avg >= 90){
+            System.out.println("A");
+        } else if (avg >= 80) {
+            System.out.println("B");
+        } else if (avg >= 70) {
+            System.out.println("C");
+        } else if (avg >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+        sc.close();
     }
 }
