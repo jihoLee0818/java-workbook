37af7ae6214f303527471658ad032f85b1f87d3e
jihoLee0818 <leejiho0114@gmail.com>
2025-08-06 19:55:38 +0900

    feat: [loops] Q5 구현

--- src/main/java/workbook1/loops/Q5.java
+++ src/main/java/workbook1/loops/Q5.java
@@ -7,5 +7,18 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         // TODO: 역삼각형 패턴 출력
+
+        //h개의 행 생성(앞에랑 똑같음)
+        for (int i = 0; i < h; i++) {
+            //행 바뀔수록 별 하나가 줄어드는 식 작성
+            //for(int j = h - i; j > 0; j--) {
+            //위의 식도 사용 가능
+            for(int j = i; j < h; j++) {
+                System.out.print("*");
+            }
+            //줄바꿈
+            System.out.println();
+        }
+        sc.close();
     }
 }
