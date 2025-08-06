c44d36b38de122c5cb4bf218f47f47adbacd40e7
jihoLee0818 <leejiho0114@gmail.com>
2025-08-06 19:18:55 +0900

    feat: [loops] Q3 구현

--- src/main/java/workbook1/loops/Q3.java
+++ src/main/java/workbook1/loops/Q3.java
@@ -7,5 +7,20 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: do-while 문으로 3의 배수의 합 계산 후 출력
+
+        //변수 선언
+        int i = 1;
+        int sum = 0;
+
+        //do-while 문을 이용하여 n보다 작은 3의 변수를 sum에 더해주는 식 작성
+        do {
+            if (i % 3 == 0)
+                sum += i;
+            i++;
+        } while (i <= n);
+
+        System.out.println(sum);
+
+        sc.close();
     }
 }
