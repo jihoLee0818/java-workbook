798b690590d52d709ac5613a4b07ed56f59f280f
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 19:56:14 +0900

    feat: [array] Q4 구현

--- src/main/java/workbook2/array/Q4.java
+++ src/main/java/workbook2/array/Q4.java
@@ -9,5 +9,31 @@ public class Q4 {
         int[][] B = new int[3][3];
         int[][] C = new int[3][3];
         // TODO: A, B에 값 저장 후 C = A + B 계산, C 출력
+
+        //3X3의 행렬을 두 개 입력
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+        //입력된 두 개의 행렬을 덧셈
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        //덧셈 완료된 행렬을 각 행마다 출력되고 줄 변환 되도록 작성
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(C[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
