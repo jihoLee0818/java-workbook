6477ff17e4905cb5e2933c8b3789639629ff5457
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 21:43:08 +0900

    feat: [methods] Q3 구현

--- src/main/java/workbook2/methods/Q3.java
+++ src/main/java/workbook2/methods/Q3.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Q3 {
     // TODO: addArrays 메서드 작성
+    public static int[] addArrays (int[] arr1, int[] arr2){
+        //새로운 배열 생성
+        int[] result = new int[arr1.length];
+
+        //반복문을 사용하여 요소를 모두 더하고 저장
+        for(int i = 0; i < arr1.length; i++){
+            result [i] = arr1 [i] + arr2 [i];
+        }
+        //배열 반환
+        return result;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,5 +27,11 @@ public class Q3 {
             arr2[i] = sc.nextInt();
         }
         // TODO: addArrays 호출 후 결과 배열 출력
+        int[] result = addArrays(arr1, arr2);
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + "\t");
+        }
+        System.out.println();
+        sc.close();
     }
 }
