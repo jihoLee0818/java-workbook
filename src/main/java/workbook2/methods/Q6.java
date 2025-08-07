4636befc16a1c8b0dee1cf33375b4225a857b259
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 22:08:41 +0900

    feat: [methods] Q6 구현

--- src/main/java/workbook2/methods/Q6.java
+++ src/main/java/workbook2/methods/Q6.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class Q6 {
     // TODO: 재귀를 이용한 factorial 메서드 작성
+    public static int factorial(int n){
+        if(n <= 1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: factorial 호출 후 결과 출력
+        int factorial = factorial(n);
+        System.out.println(factorial);
+        sc.close();
     }
 }
