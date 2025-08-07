8fa1ff298566d733119db83b743d73585fdb27d3
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 15:44:51 +0900

    feat: [breakcontinue] Q4 구현

--- src/main/java/workbook1/breakcontinue/Q4.java
+++ src/main/java/workbook1/breakcontinue/Q4.java
@@ -3,5 +3,21 @@ package main.java.workbook1.breakcontinue;
 public class Q4 {
     public static void main(String[] args) {
         // TODO: 구구단 출력 중 결과가 30 이상이면 continue
+
+        //2~9단까지 반복
+        for (int i = 2; i <= 9; i++) {
+            //* 1, * 2,......, * 9 까지 반복
+            for (int j = 1; j <= 9; j++) {
+                int result = i * j;
+                //만약 결과가 30 이상이면 넘어가기
+                if (result >= 30) {
+                    continue;
+                }
+                //결과 출력
+                System.out.print(i + "*" + j + "=" + i*j + "\t");// \t로 열 맞추기
+            }
+            //행 변환
+            System.out.println();
+        }
     }
 }
