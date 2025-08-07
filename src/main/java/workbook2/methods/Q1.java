e4c82d23ba4fdd00635ef0cb34e1bf871bde1530
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 21:01:01 +0900

    feat: [methods] Q1 구현

--- src/main/java/workbook2/methods/Q1.java
+++ src/main/java/workbook2/methods/Q1.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class Q1 {
     // TODO: 두 정수를 더해 반환하는 sum 메서드를 작성하세요.
+
+    //두 정수를 받아 합을 반환하는 메서드
+    public static int sum(int a,int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         // TODO: sum 메서드를 호출하여 결과 출력
+        System.out.println(sum(a,b));
+        sc.close();
     }
 }
