76823d65aa3d1e658dc70380063ff14dcdce7465
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 20:15:04 +0900

    feat: [array] Q5 구현

--- src/main/java/workbook2/array/Q5.java
+++ src/main/java/workbook2/array/Q5.java
@@ -8,5 +8,26 @@ public class Q5 {
         int n = sc.nextInt();
         int[] arr = new int[n];
         // TODO: arr에 값 저장 후 버블 정렬 구현, 정렬된 배열 출력
+
+        //N개의 정수를 입력받아 배열에 저장
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        //버블 정렬 알고리즘 구현
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    //버블 정렬 알고리즘
+                    //임시변수 temp를 이용하여 j번째 항과 j+1번째 항 교환
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
