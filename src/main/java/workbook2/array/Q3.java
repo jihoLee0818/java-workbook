f403cf0a6a2c0a765442ce4805b7a0245c992051
jihoLee0818 <leejiho0114@gmail.com>
2025-08-07 19:39:53 +0900

    feat: [array] Q3 구현

--- src/main/java/workbook2/array/Q3.java
+++ src/main/java/workbook2/array/Q3.java
@@ -8,5 +8,15 @@ public class Q3 {
         int n = sc.nextInt();
         int[] arr = new int[n];
         // TODO: arr에 n개의 정수 저장 후 역순으로 출력
+
+        //입력받은 정수 n의 크기를 갖는 배열 생성
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        //배열의 n번째 항부터 1번째 항까지 역순으로 출력
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
