82388f878437452b334277a1123194680c9b0e6b
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 10:18:41 +0900

    feat: [paramtypes] Q4 구현

--- src/main/java/workbook2/paramtypes/Q4.java
+++ src/main/java/workbook2/paramtypes/Q4.java
@@ -9,12 +9,16 @@ class Person {
 
 public class Q4 {
     // TODO: changeName 메서드 작성 (Person 객체와 새 이름을 받아 객체의 name 수정)
+    public static void changeName(Person person, String newName) {
+        person.name = newName;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String oldName = sc.next();
         String newName = sc.next();
         Person p = new Person(oldName);
-        // changeName(p, newName); // TODO: changeName 메서드 작성 후 주석 해제
+        changeName(p, newName); // TODO: changeName 메서드 작성 후 주석 해제
         // TODO: p.name 출력
+        System.out.println(p.name);
     }
 }
