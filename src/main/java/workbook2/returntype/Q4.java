8e01881a63aad7c3b08fc715a14e263d241a5614
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 12:26:19 +0900

    feat: [returntype] Q4 구현

--- src/main/java/workbook2/returntype/Q4.java
+++ src/main/java/workbook2/returntype/Q4.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Q4 {
     // TODO: concatStrings 메서드 작성
+    public static String concatStrings(String str1, String str2){
+        return str1 + str2;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
         // TODO: concatStrings 호출 후 결과 출력
+        String concatStrings = concatStrings(s1, s2);
+        System.out.println(concatStrings);
     }
 }
