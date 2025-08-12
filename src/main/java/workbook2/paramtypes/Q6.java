9a27abbb5eb3f685ac4e6a9fc00c0e854f1e1a7a
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 10:41:15 +0900

    feat: [paramtypes] Q6 구현

--- src/main/java/workbook2/paramtypes/Q6.java
+++ src/main/java/workbook2/paramtypes/Q6.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Q6 {
     // TODO: addExclamation(String s)와 addExclamation(StringBuilder sb) 메서드 작성
+    public static String addExclamation(String s){
+        return s + " !";
+    }
+    public static void addExclamation(StringBuilder sb){
+        sb.append("!");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         StringBuilder sb = new StringBuilder(sc.next());
-        // addExclamation(s); // TODO: addExclamation(String s) 메서드 작성 후 주석 해제
-        // addExclamation(sb); // TODO: addExclamation(StringBuilder sb) 메서드 작성 후 주석 해제
+        String modifiedS = addExclamation(s); // TODO: addExclamation(String s) 메서드 작성 후 주석 해제
+        addExclamation(sb); // TODO: addExclamation(StringBuilder sb) 메서드 작성 후 주석 해제
         // TODO: s와 sb 출력
+        System.out.println(modifiedS + " " + sb.toString());
     }
 }
