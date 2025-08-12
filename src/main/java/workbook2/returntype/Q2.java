91e5d43eaae92dc6153d936052ba924c23023f72
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 12:14:38 +0900

    feat: [returntype] Q2 구현

--- src/main/java/workbook2/returntype/Q2.java
+++ src/main/java/workbook2/returntype/Q2.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Q2 {
     // TODO: toUpper 메서드 작성 (문자열을 대문자로 반환)
+    public static String toUpper(String s){
+        return s.toUpperCase();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         // TODO: toUpper 호출 후 결과 출력
+        System.out.println(toUpper(str));
     }
 }
