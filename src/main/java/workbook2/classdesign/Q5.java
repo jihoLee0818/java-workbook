659c4ac3add013308d74c3cd0770a0386ae141cf
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 14:11:38 +0900

    feat: [classdesign] Q5 구현

--- src/main/java/workbook2/classdesign/Q5.java
+++ src/main/java/workbook2/classdesign/Q5.java
@@ -6,6 +6,22 @@ class Account {
     String accountNo;
     int balance;
     // TODO: 생성자, deposit, withdraw 메서드 작성
+    public Account(String accountNo, int balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+    public void deposit(int amount) {
+        this.balance += amount;
+        System.out.println(this.balance);
+    }
+    public void withdraw(int amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            System.out.println(this.balance);
+        } else {
+            System.out.println("잔액 부족");
+        }
+    }
 }
 
 public class Q5 {
@@ -13,20 +29,21 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         String accNo = sc.next();
         int bal = sc.nextInt();
-        // Account acc = new Account(accNo, bal); // TODO: 생성자 작성 후 주석 해제
+        Account acc = new Account(accNo, bal); // TODO: 생성자 작성 후 주석 해제
         String cmd1 = sc.next();
         int amt1 = sc.nextInt();
         if (cmd1.equals("deposit")) {
-            // acc.deposit(amt1); // TODO: deposit 메서드 작성 후 주석 해제
+            acc.deposit(amt1); // TODO: deposit 메서드 작성 후 주석 해제
         } else {
-            // acc.withdraw(amt1); // TODO: withdraw 메서드 작성 후 주석 해제
+            acc.withdraw(amt1); // TODO: withdraw 메서드 작성 후 주석 해제
         }
         String cmd2 = sc.next();
         int amt2 = sc.nextInt();
         if (cmd2.equals("deposit")) {
-            // acc.deposit(amt2); // TODO: deposit 메서드 작성 후 주석 해제
+            acc.deposit(amt2); // TODO: deposit 메서드 작성 후 주석 해제
         } else {
-            // acc.withdraw(amt2); // TODO: withdraw 메서드 작성 후 주석 해제
+            acc.withdraw(amt2); // TODO: withdraw 메서드 작성 후 주석 해제
         }
+        sc.close();
     }
 }
