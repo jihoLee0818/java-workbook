53c79fc73b3e791a6215b56b45d46007b4cfeaf0
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 14:23:43 +0900

    feat: [classdesign] Q6 구현

--- src/main/java/workbook2/classdesign/Q6.java
+++ src/main/java/workbook2/classdesign/Q6.java
@@ -6,7 +6,28 @@ class Time {
     int hour;
     int minute;
     int second;
+
     // TODO: 생성자 및 addSeconds 메서드 작성
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void addSeconds(int s) {
+        this.second += s;
+        if (this.second >= 60) {
+            this.minute = this.minute + this.second / 60;
+            this.second = this.second % 60;
+        }
+        if (this.minute >= 60) {
+            this.hour = this.hour + this.minute / 60;
+            this.minute = this.minute % 60;
+        }
+        if (this.hour >= 24) {
+            this.hour = this.hour % 24;
+        }
+    }
 }
 
 public class Q6 {
@@ -16,8 +37,10 @@ public class Q6 {
         int m = sc.nextInt();
         int s = sc.nextInt();
         int add = sc.nextInt();
-        // Time time = new Time(h, m, s); // TODO: 생성자 작성 후 주석 해제
-        // time.addSeconds(add); // TODO: addSeconds 메서드 작성 후 주석 해제
+        Time time = new Time(h, m, s); // TODO: 생성자 작성 후 주석 해제
+        time.addSeconds(add); // TODO: addSeconds 메서드 작성 후 주석 해제
         // TODO: time.hour time.minute time.second 출력
+        System.out.println(time.hour + " " + time.minute + " " + time.second);
+        sc.close();
     }
 }
