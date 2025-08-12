0b015945279aed1d195addf99865a3ec2dfaf233
jihoLee0818 <leejiho0114@gmail.com>
2025-08-12 12:13:02 +0900

    feat: [returntype] Q1 구현

--- src/main/java/workbook2/returntype/Q1.java
+++ src/main/java/workbook2/returntype/Q1.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Q1 {
     // TODO: add 메서드 작성
+    public static int add(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         // TODO: add 호출 후 결과 출력
+        System.out.println(add(a,b));
     }
 }
