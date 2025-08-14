368f079b4f74d080833dd5b40e288e679f7e2e1e
jihoLee0818 <leejiho0114@gmail.com>
2025-08-14 16:56:59 +0900

    feat: [methodcompare] Q4 구현

--- src/main/java/workbook2/methodcompare/Q4.java
+++ src/main/java/workbook2/methodcompare/Q4.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 class StringUtil {
     static boolean isPalindrome(String s) {
         // TODO: 회문 여부 판단
-        return false;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -14,5 +19,6 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         // TODO: StringUtil.isPalindrome 호출 후 출력
+        System.out.println(StringUtil.isPalindrome(str));
     }
 }
