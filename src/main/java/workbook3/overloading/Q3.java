12d750184896a091a638021507c9e0842fb359d3
이지호 <leejiho0114@gmail.com>
2025-08-26 21:43:46 +0900

    feat: [overloading] Q3 구현

--- src/main/java/workbook3/overloading/Q3.java
+++ src/main/java/workbook3/overloading/Q3.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class Q3 {
     // TODO: concat 메서드 오버로딩
+    public static String concat(String s1, String s2) {
+        return s1 + s2;
+    }
+
+    public static String concat(String s1, String s2, String s3) {
+        return concat(concat(s1, s2), s3);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
         String s3 = sc.next();
         // TODO: concat 메서드 작성 후 아래 주석 해제
-        // System.out.println(concat(s1,s2));
-        // System.out.println(concat(s1,s2,s3));
+        System.out.println(concat(s1, s2));
+        System.out.println(concat(s1, s2, s3));
+        sc.close();
     }
 }
