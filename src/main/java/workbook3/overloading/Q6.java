85a636fffc7f5d0ce7c77725303a8d3e508ae77f
이지호 <leejiho0114@gmail.com>
2025-08-26 22:57:27 +0900

    feat: [overloading] Q6 구현

--- src/main/java/workbook3/overloading/Q6.java
+++ src/main/java/workbook3/overloading/Q6.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class Q6 {
     // TODO: multiply 오버로딩 (int[]), (int[][], int[][])
+    public static int multiply(int[] arr) {
+        int product = 1;
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+        return product;
+    }
+
+    public static int[][] multiply(int[][] m1, int[][] m2) {
+        int size = m1.length;
+        int[][] result = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    result[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int type = sc.nextInt();
@@ -11,10 +32,11 @@ public class Q6 {
             int n = sc.nextInt();
             int[] arr = new int[n];
             for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
-            // System.out.println(multiply(arr)); // TODO: multiply(int[]) 메서드 작성 후 주석 해제
+            System.out.println(multiply(arr)); // TODO: multiply(int[]) 메서드 작성 후 주석 해제
         } else {
             int[][] m1 = new int[2][2];
             int[][] m2 = new int[2][2];
+
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 2; j++) {
                     m1[i][j] = sc.nextInt();
@@ -24,12 +46,11 @@ public class Q6 {
                 for (int j = 0; j < 2; j++) {
                     m2[i][j] = sc.nextInt();
                 }
-                // TODO: multiply(int[][], int[][]) 메서드 작성 후 주석 해제 아래 주석 해제
-                // int[][] result = multiply(m1, m2);
             }
+            int[][] result = multiply(m1, m2);
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 2; j++) {
-                   // System.out.print(result[i][j] + (j==1?"":" "));
+                    System.out.print(result[i][j] + (j == 1 ? "" : " "));
                 }
                 System.out.println();
             }
