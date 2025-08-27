49d6c1e3d993b9747b5bced30ba9c51d28af65e3
이지호 <leejiho0114@gmail.com>
2025-08-27 17:12:23 +0900

    feat: [constructor] Q1 구현

--- src/main/java/workbook3/constructor/Q1.java
+++ src/main/java/workbook3/constructor/Q1.java
@@ -6,9 +6,11 @@ class Student {
     String name;
     Student() {
         // TODO: name 초기화
+        this.name = "Unknown";
     }
     Student(String name) {
         // TODO: name 초기화
+        this.name = name;
     }
 }
 
@@ -19,5 +21,7 @@ public class Q1 {
         Student s1 = new Student();
         Student s2 = new Student(name);
         // TODO: s1.name과 s2.name 출력
+        System.out.println(s1.name);
+        System.out.println(s2.name);
     }
 }
