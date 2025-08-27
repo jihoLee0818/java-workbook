96ba279ca01cdfdb78a9b3d4047a89cdbe83431d
이지호 <leejiho0114@gmail.com>
2025-08-27 18:36:46 +0900

    feat: [varinit] Q4 구현

--- src/main/java/workbook3/varinit/Q4.java
+++ src/main/java/workbook3/varinit/Q4.java
@@ -4,11 +4,18 @@ import java.util.Random;
 
 class RandomInit {
     int[] arr = new int[5];
+
     {
         // TODO: arr 배열을 난수로 채움
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10);
+        }
     }
+
     void printArray() {
         // TODO: arr 요소 출력
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
     }
 }
 
