8b90231b72dee34331ad0db3391d0ac9bf17fc97
이지호 <leejiho0114@gmail.com>
2025-08-27 18:26:45 +0900

    feat: [varinit] Q2 구현

--- src/main/java/workbook3/varinit/Q2.java
+++ src/main/java/workbook3/varinit/Q2.java
@@ -4,6 +4,7 @@ public class Q2 {
     static int count;
     static {
         // TODO: count 초기화
+        count = 5;
     }
     public static void main(String[] args) {
         System.out.println(count);
